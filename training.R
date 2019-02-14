62a546dedc90f815396bbdccbabee46f87b0869b
Gaio <jhonguedes@gmail.com>
2019-02-14 18:31:15 -0300

    New plots

--- training.R
+++ training.R
@@ -24,6 +24,33 @@ ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy, shape = class)
 
 ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
 
+# Plot facets for categorical variables
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_wrap(~ class, nrow = 2) # Add separated plots to each category on 'class'
 
-ggplot(data = mpg) + geom_point(mapping = aes(x = hwy, y = cyl))
-ggplot(data = mpg) + geom_point(mapping = aes(x = drv, y = class))
+# Plot facets grouped by combination of two variables
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(drv ~ cyl)
+
+# Plot facets grouped by one variable (simillar to facet_wrap with one variable)
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(. ~ cyl)
+
+# Plot differente geoms - plots
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x = displ, y = hwy, color = drv, linetype = drv)) +
+  geom_point(mapping = aes(x = displ, y = hwy, color = drv))
+
+# The same plot before, but with info into ggplot function
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_smooth() +
+  geom_point()
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(mapping = aes(color = class)) +
+  geom_smooth()
+
+str(mpg)
